322363a4c98029cabe8363427ae234df0e3f4d4f
Suzuki Takafumi <suzukitakafumifcc@gmail.com>
2024-04-18 16:00:45 +0900

    chore: migration test

--- database/migrations/2024_04_18_155716_delete_test_column.php
+++ database/migrations/2024_04_18_155716_delete_test_column.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('test')->after('email');
+            $table->dropColumn('test');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('test');
+            $table->string('test')->after('email');
         });
     }
 };
