8fe28490f9db2728ca49bf248daf46a59fe2c41e
Suzuki Takafumi <suzukitakafumifcc@gmail.com>
2024-04-18 17:44:44 +0900

    feat: 一覧表示

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,1 +1,19 @@
-<x-app-layout></x-app-layout>>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            一覧表示
+        </h2>
+    </x-slot>
+    <div class="mx-auto px-6">
+        @foreach ($posts as $post)
+            <div class="mt-4 p-8 bg-white w-full rounded-2xl">
+                <h1 class="p-4 text-lg font-semibold">{{ $post->title }}</h1>
+                <hr class="w-full">
+                <p class="mt-4 p-4">{{ $post->body }}</p>
+                <div class="p-4 text-sm font-semibold">
+                    {{ $post->created_at }}
+                </div>
+            </div>
+        @endforeach
+    </div>
+</x-app-layout>
